2e2d6fa41feb319e740eef44cdb5b6275ffd1c8e
hockeymiass21@gmail.com <hockeymiass21@gmail.com>
2022-10-03 22:41:02 +0500

    ешили первую задачу2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
 Console.Write("Введите имя: ");
 string name = Console.ReadLine();
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine($"Квадрат {number} равен {number*number}");
 Console.WriteLine("Ваше имя "+name);
-
+*/
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a*b==a)
+    Console.WriteLine($" {a} квадрат {b}");
+else
+    Console.WriteLine("Нет не является");
+    

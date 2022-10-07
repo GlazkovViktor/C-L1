2f214ab8766e1d926f3b1468cbffae356834db44
hockeymiass21@gmail.com <hockeymiass21@gmail.com>
2022-10-07 10:50:22 +0500

    решили первую задачу

---
+++ sem2/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(10,99);
+int s1 = num / 10;
+int s2 = num % 10;
+Console.WriteLine(num);
+Console.WriteLine(s1+" "+s2);
+if (s1>s2) Console.WriteLine(s1);
+else Console.WriteLine(s2);

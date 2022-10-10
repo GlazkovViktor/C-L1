891f4de188d5bb3aab0582becf1c02bef6df348b
hockeymiass21@gmail.com <hockeymiass21@gmail.com>
2022-10-11 00:11:01 +0500

    решил 2 задачу семинар 3 дз

--- sem2/Program.cs
+++ sem2/Program.cs
@@ -136,9 +136,9 @@ double dist (int x1, int y1, int x2, int y2, int z1, int z2){
     }
     int x1 = int.Parse(Console.ReadLine());
     int y1 = int.Parse(Console.ReadLine());
-    int x2 = int.Parse(Console.ReadLine());
-    int y2 = int.Parse(Console.ReadLine());
     int z1 = int.Parse(Console.ReadLine());
+    int x2 = int.Parse(Console.ReadLine());
+    int y2 = int.Parse(Console.ReadLine()); 
     int z2 = int.Parse(Console.ReadLine());
     double otvet = dist(x1,y1,x2,y2,z1,z2);
-    Console.WriteLine(otvet);*
+    Console.WriteLine(otvet);
